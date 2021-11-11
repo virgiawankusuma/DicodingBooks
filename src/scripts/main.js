bb66ffdfebf50f570356fddd7df48f5c77c0d053
virgiawankusuma <virgiawan150@gmail.com>
2021-11-11 17:44:12 +0700

    fix: mengubah fungsi insertBook menjadi Fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,27 +24,24 @@ function main() {
 
 
     const insertBook = (book) => {
-        // setup callback if response succes or error
-        xhr.onload = function () {
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
-
-        xhr.onerror = function () {
-            showResponseMessage();
-        }
-
-        // Making POST request and setup URL target
-        xhr.open("POST", `${baseUrl}/add`);
-
-        // Setup Content-Type property and X-Auth-Token in Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        // Send request and attach JSON.stringify(book) in body
-        xhr.send(JSON.stringify(book));
-        // console.log(JSON.stringify(book));
+        fetch(`${baseUrl}/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth-Token': '12345'
+            },
+            body: JSON.stringify(book)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(responseJson => {
+                showResponseMessage(responseJson.message);
+                getBook();
+            })
+            .catch(error => {
+                showResponseMessage(error);
+            })
     };
 
     const updateBook = (book) => {
